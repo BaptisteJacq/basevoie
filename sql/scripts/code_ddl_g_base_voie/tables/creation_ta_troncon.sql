046f3f6e32330d74f180a2bd66e45ac6e84995d5
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2022-10-14 12:00:18 +0200

    Correction du code DDL des tables définitives de la Base Voie
    - Remplacement de TA_VOIE par TA_VOIE_PHYSIQUE et TA_VOIE_ADMINISTRATIVE ;
    - Modification des tables de log en conséquence ;
    - Ajout de TA_VOIE_SUPRA_COMMUNALE ;

--- sql/scripts/code_ddl_g_base_voie/tables/creation_ta_troncon.sql
+++ sql/scripts/code_ddl_g_base_voie/tables/creation_ta_troncon.sql
@@ -1,33 +1,31 @@
 /*
-La table TA_TRONCON regroupe tous les tronçons de la base voie.
+La table TA_TRONCON - du projet C de correction de la latéralité des voies - regroupe tous les tronçons de la base voie.
 */
 
 -- 1. Création de la table TA_TRONCON
 CREATE TABLE G_BASE_VOIE.TA_TRONCON(
     objectid NUMBER(38,0),
-    geom SDO_GEOMETRY NOT NULL,
-    sens CHAR(1 BYTE),
-    ordre_troncon NUMBER(2,0),
-    date_saisie DATE DEFAULT sysdate NOT NULL,
-    date_modification DATE DEFAULT sysdate NOT NULL,
-    fid_voie NUMBER(38,0),
-    fid_pnom_saisie NUMBER(38,0) NOT NULL,
-    fid_pnom_modification NUMBER(38,0) NOT NULL,
-    fid_metadonnee NUMBER(38,0) NULL
+    ordre_troncon NUMBER(2),
+    date_saisie DATE DEFAULT sysdate NULL,
+    date_modification DATE DEFAULT sysdate NULL,
+    fid_pnom_saisie NUMBER(38,0) NULL,
+    fid_pnom_modification NUMBER(38,0) NULL,
+    fid_etat NUMBER(38,0) NULL,
+    fid_voie_physique NUMBER(38,0),
+    geom SDO_GEOMETRY NOT NULL
 );
 
 -- 2. Création des commentaires sur la table et les champs
-COMMENT ON TABLE G_BASE_VOIE.TA_TRONCON IS 'Table contenant les tronçons de la base voie. Les tronçons sont les objets de base de la base voie servant à constituer les rues qui elles-mêmes constituent les voies. Ancienne table : ILTATRC.';
+COMMENT ON TABLE G_BASE_VOIE.TA_TRONCON IS 'Table contenant tous les tronçons de la base voie.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.objectid IS 'Clé primaire de la table identifiant chaque tronçon. Cette pk est auto-incrémentée et remplace l''ancien identifiant cnumtrc.';
-COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.geom IS 'Géométrie de type ligne simple de chaque tronçon.';
-COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.sens IS 'Code permettant de connaître le sens de saisie du tronçon par rapport au sens de la voie : + = dans le sens de la voie ; - = dans le sens inverse de la voie.';
-COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.ordre_troncon IS 'Ordre dans lequel les tronçons se positionnent afin de constituer la voie. 1 est égal au début de la voie et 1 + n est égal au tronçon suivant.';
+COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.ordre_troncon IS 'Ordre dans lequel les tronçons se positionnent afin de contituer la voie administrative. 1 est égal au début de la voie et 1 + n est égal au tronçon suivant.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.date_saisie IS 'date de saisie du tronçon (par défaut la date du jour).';
 COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.date_modification IS 'Dernière date de modification du tronçon (par défaut la date du jour).';
 COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.fid_pnom_saisie IS 'Clé étrangère vers la table TA_AGENT permettant de récupérer le pnom de l''agent ayant créé un tronçon.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.fid_pnom_modification IS 'Clé étrangère vers la table TA_AGENT permettant de récupérer le pnom de l''agent ayant modifié un tronçon.';
-COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.fid_voie IS 'Clé étrangère vers la table TA_VOIE permettant d''associer une voie à un ou plusieurs tronçons. Ancien champ : CCOMVOI.';
-COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.fid_metadonnee IS 'Clé étrangère vers la table G_GEO.TA_METADONNEE permettant de connaître la source des tronçons (MEL ou IGN).';
+COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.fid_etat IS 'Etat d''avancement des corrections : en erreur, corrigé, correct.';
+COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.fid_voie_physique IS 'Clé étrangère vers la table TA_VOIE_PHYSIQUE permettant d''associer un tronçon à une et une seule voie physique.';
+COMMENT ON COLUMN G_BASE_VOIE.TA_TRONCON.geom IS 'Géométrie de type ligne simple de chaque tronçon.';
 
 -- 3. Création de la clé primaire
 ALTER TABLE G_BASE_VOIE.TA_TRONCON 
@@ -44,7 +42,7 @@ INSERT INTO USER_SDO_GEOM_METADATA(
 )
 VALUES(
     'TA_TRONCON',
-    'geom',
+    'GEOM',
     SDO_DIM_ARRAY(SDO_DIM_ELEMENT('X', 684540, 719822.2, 0.005),SDO_DIM_ELEMENT('Y', 7044212, 7078072, 0.005)), 
     2154
 );
@@ -52,29 +50,29 @@ VALUES(
 -- 5. Création de l'index spatial sur le champ geom
 CREATE INDEX TA_TRONCON_SIDX
 ON G_BASE_VOIE.TA_TRONCON(GEOM)
-INDEXTYPE IS MDSYS.SPATIAL_INDEX_V2
+INDEXTYPE IS MDSYS.SPATIAL_INDEX
 PARAMETERS('sdo_indx_dims=2, layer_gtype=LINE, tablespace=G_ADT_INDX, work_tablespace=DATA_TEMP');
 
 -- 6. Création des clés étrangères
 ALTER TABLE G_BASE_VOIE.TA_TRONCON
 ADD CONSTRAINT TA_TRONCON_FID_PNOM_SAISIE_FK 
 FOREIGN KEY (fid_pnom_saisie)
-REFERENCES G_BASE_VOIE.ta_agent(numero_agent);
+REFERENCES G_BASE_VOIE.TA_AGENT(numero_agent);
 
 ALTER TABLE G_BASE_VOIE.TA_TRONCON
 ADD CONSTRAINT TA_TRONCON_FID_PNOM_MODIFICATION_FK
 FOREIGN KEY (fid_pnom_modification)
-REFERENCES G_BASE_VOIE.ta_agent(numero_agent);
+REFERENCES G_BASE_VOIE.TA_AGENT(numero_agent);
 
 ALTER TABLE G_BASE_VOIE.TA_TRONCON
-ADD CONSTRAINT TA_TRONCON_FID_VOIE_FK
-FOREIGN KEY (fid_voie)
-REFERENCES G_BASE_VOIE.TA_VOIE(objectid);
+ADD CONSTRAINT TA_TRONCON_FID_ETAT_FK
+FOREIGN KEY (fid_etat)
+REFERENCES G_BASE_VOIE.TA_LIBELLE(objectid);
 
 ALTER TABLE G_BASE_VOIE.TA_TRONCON
-ADD CONSTRAINT TA_TRONCON_FID_METADONNEE_FK
-FOREIGN KEY (fid_metadonnee)
-REFERENCES G_GEO.ta_metadonnee(objectid);
+ADD CONSTRAINT TA_TRONCON_FID_VOIE_PHYSIQUE_FK
+FOREIGN KEY (fid_voie_physique)
+REFERENCES G_BASE_VOIE.TA_VOIE_PHYSIQUE(objectid);
 
 -- 7. Création des index sur les clés étrangères et autres
 CREATE INDEX TA_TRONCON_FID_PNOM_SAISIE_IDX ON G_BASE_VOIE.TA_TRONCON(fid_pnom_saisie)
@@ -83,10 +81,13 @@ CREATE INDEX TA_TRONCON_FID_PNOM_SAISIE_IDX ON G_BASE_VOIE.TA_TRONCON(fid_pnom_s
 CREATE INDEX TA_TRONCON_FID_PNOM_MODIFICATION_IDX ON G_BASE_VOIE.TA_TRONCON(fid_pnom_modification)
     TABLESPACE G_ADT_INDX;
 
-CREATE INDEX TA_TRONCON_FID_VOIE_IDX ON G_BASE_VOIE.TA_TRONCON(fid_voie)
+CREATE INDEX TA_TRONCON_FID_ETAT_IDX ON G_BASE_VOIE.TA_TRONCON(fid_etat)
     TABLESPACE G_ADT_INDX;
 
-CREATE INDEX TA_TRONCON_FID_METADONNEE_IDX ON G_BASE_VOIE.TA_TRONCON(fid_metadonnee)
+CREATE INDEX TA_TRONCON_FID_VOIE_PHYSIQUE_IDX ON G_BASE_VOIE.TA_TRONCON(fid_voie_physique)
+    TABLESPACE G_ADT_INDX;
+
+CREATE INDEX TA_TRONCON_FID_VOIE_PHYSIQUE_IDX ON G_BASE_VOIE.TA_TRONCON(ordre_troncon)
     TABLESPACE G_ADT_INDX;
 
 -- Cet index dispose d'une fonction permettant d'accélérer la récupération du code INSEE de la commune d'appartenance du tronçon. 
@@ -99,4 +100,3 @@ TABLESPACE G_ADT_INDX;
 GRANT SELECT ON G_BASE_VOIE.TA_TRONCON TO G_ADMIN_SIG;
 
 /
-
