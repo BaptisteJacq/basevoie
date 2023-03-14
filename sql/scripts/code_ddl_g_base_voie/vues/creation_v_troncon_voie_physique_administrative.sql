c4f1b9d1aa900a5b8dcf0e96a3d5edb854d7e202
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2023-03-14 14:28:05 +0100

    mise à jour de la vue V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE au "format" bdTopo
    création des champs id_voie_droite, id_voie_gauche, nom_voie_droite, nom_voie_gauche, etc

--- sql/scripts/code_ddl_g_base_voie/vues/creation_v_troncon_voie_physique_administrative.sql
+++ sql/scripts/code_ddl_g_base_voie/vues/creation_v_troncon_voie_physique_administrative.sql
@@ -7,40 +7,37 @@ DELETE FROM USER_SDO_GEOM_METADATA WHERE TABLE_NAME = 'V_TRONCON_VOIE_PHYSIQUE_A
 COMMIT;
 */
 -- 1. Création de la vue
-CREATE OR REPLACE FORCE VIEW "G_BASE_VOIE"."V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE" (TRONCON_GEOM, TRONCON_OBJECTID, TRONCON_DATE_SAISIE, TRONCON_DATE_MODIFICATION, TRONCON_FID_VOIE_PHYSIQUE, VOIE_PHYSIQUE_OBJECTID, VOIE_PHYSIQUE_FID_ACTION, RELATION_VOIE_PHYSIQUE_ADMINISTRATIVE_FID_LATERALITE, TYPE_VOIE_LIBELLE, VOIE_ADMINISTRATIVE_OBJECTID, VOIE_ADMINISTRATIVE_GENRE_VOIE, VOIE_ADMINISTRATIVE_LIBELLE_VOIE, VOIE_ADMINISTRATIVE_COMPLEMENT_NOM_VOIE, VOIE_ADMINISTRATIVE_CODE_INSEE, VOIE_ADMINISTRATIVE_COMMENTAIRE, VOIE_ADMINISTRATIVE_DATE_SAISIE, VOIE_ADMINISTRATIVE_DATE_MODIFICATION, RIVOLI_CODE, RIVOLI_CLE_CONTROLE,
-    CONSTRAINT "V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE_PK" PRIMARY KEY ("TRONCON_OBJECTID") DISABLE) AS 
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "G_BASE_VOIE"."V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE" (GEOM, ID_TRONCON, ID_VOIE_PHYSIQUE, ACTION_VOIE_PHYSIQUE, ID_VOIE_DROITE, ID_VOIE_GAUCHE, NOM_VOIE_DROITE, NOM_VOIE_GAUCHE, CODE_INSEE_VOIE_DROITE, CODE_INSEE_VOIE_GAUCHE, COMMENTAIRE_VOIE_DROITE, COMMENTAIRE_VOIE_GAUCHE, 
+	CONSTRAINT "V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE_PK" PRIMARY KEY ("ID_TRONCON") DISABLE) AS 
 SELECT
-    a.geom AS troncon_geom,
-	a.objectid AS troncon_objectid,
-	a.date_saisie AS troncon_date_saisie,
-	a.date_modification AS troncon_date_modification,
-	a.fid_voie_physique AS troncon_fid_voie_physique,
-	b.objectid AS voie_physique_objectid,
-	g.libelle_court AS voie_physique_fid_action,
-	h.libelle_court AS relation_voie_physique_administrative_fid_lateralite,
-	e.libelle AS type_voie_libelle,
-	d.objectid AS voie_administrative_objectid,
-	d.genre_voie AS voie_administrative_genre_voie,
-	d.libelle_voie AS voie_administrative_libelle_voie,
-	d.complement_nom_voie AS voie_administrative_complement_nom_voie,
-	d.code_insee AS voie_administrative_code_insee,
-	d.commentaire AS voie_administrative_commentaire,
-	d.date_saisie AS voie_administrative_date_saisie,
-	d.date_modification AS voie_administrative_date_modification,
-	f.code_rivoli AS rivoli_code,
-	f.cle_controle AS rivoli_cle_controle
+    a.geom,
+	a.objectid AS id_troncon,
+	b.objectid AS id_voie_physique,
+	g.libelle_court AS action_voie_physique,
+	d.objectid AS id_voie_droite,
+    j.objectid AS id_voie_gauche,
+	CAST(SUBSTR(UPPER(TRIM(e.libelle)), 1, 1) || SUBSTR(LOWER(TRIM(e.libelle)), 2) || CASE WHEN d.libelle_voie IS NOT NULL THEN ' ' || TRIM(d.libelle_voie) ELSE '' END || CASE WHEN d.complement_nom_voie IS NOT NULL THEN ' ' || TRIM(d.complement_nom_voie) ELSE '' END || CASE WHEN d.code_insee = '59298' THEN ' (Hellemmes-Lille)' WHEN d.code_insee = '59355' THEN ' (Lomme)' END AS VARCHAR2(254)) AS nom_voie_droite,
+    CAST(SUBSTR(UPPER(TRIM(k.libelle)), 1, 1) || SUBSTR(LOWER(TRIM(k.libelle)), 2) || CASE WHEN j.libelle_voie IS NOT NULL THEN ' ' || TRIM(j.libelle_voie) ELSE '' END || CASE WHEN j.complement_nom_voie IS NOT NULL THEN ' ' || TRIM(j.complement_nom_voie) ELSE '' END || CASE WHEN j.code_insee = '59298' THEN ' (Hellemmes-Lille)' WHEN j.code_insee = '59355' THEN ' (Lomme)' END AS VARCHAR2(254)) AS nom_voie_gauche,
+	d.code_insee AS code_insee_voie_droite,
+    j.code_insee AS code_insee_voie_gauche,
+	d.commentaire AS commentaire_voie_droite,
+    j.commentaire AS commentaire_voie_gauche
 FROM
 	G_BASE_VOIE.TEMP_I_TRONCON a
 	INNER JOIN G_BASE_VOIE.TEMP_I_VOIE_PHYSIQUE b ON b.objectid = a.fid_voie_physique
 	INNER JOIN G_BASE_VOIE.TEMP_I_RELATION_VOIE_PHYSIQUE_ADMINISTRATIVE c ON c.fid_voie_physique = b.objectid
 	INNER JOIN G_BASE_VOIE.TEMP_I_VOIE_ADMINISTRATIVE d ON d.objectid = c.fid_voie_administrative
 	INNER JOIN G_BASE_VOIE.TEMP_I_TYPE_VOIE e ON e.objectid = d.fid_type_voie
-	LEFT JOIN G_BASE_VOIE.TEMP_I_RIVOLI f ON f.objectid = d.fid_rivoli
-	INNER JOIN G_BASE_VOIE.TEMP_I_LIBELLE g ON g.objectid = b.fid_action
-	INNER JOIN G_BASE_VOIE.TEMP_I_LIBELLE h ON h.objectid = c.fid_lateralite;
+	INNER JOIN G_BASE_VOIE.TEMP_I_LIBELLE g ON g.objectid = b.fid_action   
+    INNER JOIN G_BASE_VOIE.TEMP_I_RELATION_VOIE_PHYSIQUE_ADMINISTRATIVE i ON i.fid_voie_physique = b.objectid
+	INNER JOIN G_BASE_VOIE.TEMP_I_VOIE_ADMINISTRATIVE j ON j.objectid = i.fid_voie_administrative
+	INNER JOIN G_BASE_VOIE.TEMP_I_TYPE_VOIE k ON k.objectid = j.fid_type_voie
+WHERE
+    c.fid_lateralite IN(1,3)
+    AND i.fid_lateralite IN(2,3);
 
 -- 2. Création des commentaires
-COMMENT ON TABLE G_BASE_VOIE.V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE IS 'Vue compilant toutes les informations des tronçons, voies physiques et voies administratives de la nouvelle structure base voie.';
+COMMENT ON TABLE G_BASE_VOIE.V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE IS 'Vue compilant toutes les informations des tronçons, voies physiques et voies administratives de la nouvelle structure base voie au "format" BdTopo.';
 
 -- 3. Création des métadonnées spatiales
 INSERT INTO USER_SDO_GEOM_METADATA(
@@ -51,7 +48,7 @@ INSERT INTO USER_SDO_GEOM_METADATA(
 )
 VALUES(
     'V_TRONCON_VOIE_PHYSIQUE_ADMINISTRATIVE',
-    'TRONCON_GEOM',
+    'GEOM',
     SDO_DIM_ARRAY(SDO_DIM_ELEMENT('X', 684540, 719822.2, 0.005),SDO_DIM_ELEMENT('Y', 7044212, 7078072, 0.005)), 
     2154
 );
